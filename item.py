28acdf1c25a6651afa5970bb1a20c30d4b393e44
Batman (Antoeknee) <antcasawesome123@gmail.com>
2026-03-26 03:09:55 +0000

    Finished and added some getter and setters for the names

--- item.py
+++ item.py
@@ -2,6 +2,16 @@ class item():
     def __init__(self, name, description):
         self.name = name
         self.description = None
+
+
+
+def get_name (self, new_name):
+    self.get_name = new_name
+
+    
+def set_name (self, new_name):   
+    if new_name != "":
+        print("This is an invalid name.")
+
+
         
-    
-    
